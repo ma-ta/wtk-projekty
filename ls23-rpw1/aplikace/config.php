0e8fc692de1234c6d9d53d52d36c1a4c68f2ef2d
Martin TÁBOR <ma-ta@users.noreply.github.com>
2024-05-24 08:19:22 +0200

    název stránky (domény)
    TODO: implementovat automatickou detekci

--- ls23-rpw1/aplikace/config.php
+++ ls23-rpw1/aplikace/config.php
@@ -4,9 +4,12 @@
 /* K O N F I G U R A C E */
 /*************************/
 
+/* TODO: název získávat automatizovaně např. z HTTP hlavičky
+   jedna instance aplikace (1 hosting) by měla podporovat mnoho domén (pomocí přesměrování) */
+define("DOMENA",        "DrahaDomena.com");            // zobrazovaný název domény (k prodeji)
+
 define("ADRESA_WEBU",   "//localhost/webprojekt");     // url adresa stránek
 define("JMENO_AUTORA",  "Martin TÁBOR");               // jméno provozovatele stránek (resp. vlatníka) v patičce webu
-define("DOMENA",        "DrahaDomena.com");            // zobrazovaný název domény (k prodeji)
 define("ADRESA_MAILU",  "tym@" . strtolower(DOMENA));  // zobrazovaná e-mailová kontaktní adresa provozovatele (majitele)
 define("NADPIS",        DOMENA);                       // nadpis home page
 define("NAZEV_STRANKY", DOMENA);                       // title v hlavičce stránky
